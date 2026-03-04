aedb125a63fc86fd7003040e52b31c7b087b5897
Ashishkumar-alt <hansraj986532@gmail.com>
2026-03-04 17:06:20 +0530

    deploy

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: process.cwd(),
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
